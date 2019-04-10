f23679c7c2210d5f9e9b5f55d1c42e7bf4ed1490
KorbinianBstrtr <ga84can@mytum.de>
2019-04-10 08:04:03 +0200

    statistical

--- experiments/rotation/run_experiment_statistic.py
+++ experiments/rotation/run_experiment_statistic.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-#SBATCH --gpus=220
+#SBATCH --gpus=80
 #SBATCH --mem=12GB
 #SBATCH --time=13:30:00
 #SBATCH --mail-user=abstreik
@@ -17,35 +17,35 @@ train_range = range(14, 26, 1)
 n_runs = 20
 
 def mp_worker(data):
-    lossfnstr, train_seed, n_train = data
+    lossfnstr, train_seed = data
     loss_fn = [{'loss_fn': lossfn.get_mse_loss(), 'weight': 1, 'label': 'mse'}]
     if lossfnstr == 'det' or lossfnstr == 'detnorm':
         loss_fn.append({'loss_fn': lossfn.get_det_loss(), 'weight': 0.2, 'label': 'det'})
     if lossfnstr == 'norm' or lossfnstr == 'detnorm':
         loss_fn.append({'loss_fn': lossfn.get_norm_loss(), 'weight': 0.2, 'label': 'norm'})
 
-    checkpoint_dir = 'checkpoints/'
-    checkpoint_dir += 'checkpoints_statistical/'
-    checkpoint_dir += 'checkpoint_dim-{}_ntrain-{}_lossfn-{}_seed-{}/'.format(dim, n_train, lossfnstr,
-                                                                                  train_seed)
-    run_experiment(dim, n_train, train_seed, loss_fn, 0, checkpoint_dir)
+    train_range_spec = train_range
+    if lossfnstr == 'error':
+        train_range_spec = range(20, 20, 1)
+    if lossfnstr == 'det':
+        train_range_spec = range(15, 20, 1)
+    if lossfnstr == 'detnorm':
+        train_range_spec = range(14, 20, 1)
+    if lossfnstr == 'norm':
+        train_range_spec = range(17, 20, 1)
+    for n_train in train_range_spec:
+        checkpoint_dir = 'checkpoints/'
+        checkpoint_dir += 'checkpoints_statistical/'
+        checkpoint_dir += 'checkpoint_dim-{}_ntrain-{}_lossfn-{}_seed-{}/'.format(dim, n_train, lossfnstr,
+                                                                                      train_seed)
+        run_experiment(dim, n_train, train_seed, loss_fn, 0, checkpoint_dir)
 
 
 def mp_handler():
     for lossfnstr in loss_fns:
         for train_seed in range(1683, 1683 + n_runs, 1):
-            train_range_spec = train_range
-            if lossfnstr == 'error':
-                train_range_spec = range(20, 26, 1)
-            if lossfnstr == 'det':
-                train_range_spec = range(15, 26, 1)
-            if lossfnstr == 'detnorm':
-                train_range_spec = range(14, 26, 1)
-            if lossfnstr == 'norm':
-                train_range_spec = range(17, 26, 1)
-            for n_train in train_range_spec:
-                p = multiprocessing.Process(target=mp_worker, args=((lossfnstr, train_seed, n_train),))
-                p.start()
+            p = multiprocessing.Process(target=mp_worker, args=((lossfnstr, train_seed),))
+            p.start()
 
 
 if __name__ == '__main__':
