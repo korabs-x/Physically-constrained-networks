0bf9b64dca41193a7f2b7e07291f06c1a46e48d9
KorbinianBstrtr <ga84can@mytum.de>
2019-04-14 19:12:31 +0200

    add statistical calculations

--- experiments/rotation/run_experiment_statistic.py
+++ experiments/rotation/run_experiment_statistic.py
@@ -10,10 +10,10 @@ import multiprocessing
 import lossfn
 from experiment import run_experiment
 
-loss_fns = ['detnorm', 'det', 'norm', 'error']# ['error', 'det', 'norm', 'detnorm']
+loss_fns = ['error', 'det', 'detnorm', 'norm']# ['error', 'det', 'norm', 'detnorm']
 
 dim = 2
-train_range = range(14, 26, 1)
+train_range = range(1, 26, 1)
 n_runs = 20
 
 def mp_worker(data):
@@ -22,20 +22,20 @@ def mp_worker(data):
     if lossfnstr == 'det' or lossfnstr == 'detnorm':
         loss_fn.append({'loss_fn': lossfn.get_det_loss(), 'weight': 0.2, 'label': 'det'})
     if lossfnstr == 'norm' or lossfnstr == 'detnorm':
-        loss_fn.append({'loss_fn': lossfn.get_norm_loss(), 'weight': 0.2, 'label': 'norm'})
+        loss_fn.append({'loss_fn': lossfn.get_norm_loss(), 'weight': 1, 'label': 'norm'})
 
     train_range_spec = train_range
-    if lossfnstr == 'error':
+    """if lossfnstr == 'error':
         train_range_spec = range(20, 20, 1)
     if lossfnstr == 'det':
         train_range_spec = range(15, 20, 1)
     if lossfnstr == 'detnorm':
         train_range_spec = range(14, 20, 1)
     if lossfnstr == 'norm':
-        train_range_spec = range(17, 20, 1)
+        train_range_spec = range(17, 20, 1)"""
     for n_train in train_range_spec:
         checkpoint_dir = 'checkpoints/'
-        checkpoint_dir += 'checkpoints_statistical/'
+        checkpoint_dir += 'checkpoints_statistical2/'
         checkpoint_dir += 'checkpoint_dim-{}_ntrain-{}_lossfn-{}_seed-{}/'.format(dim, n_train, lossfnstr,
                                                                                       train_seed)
         run_experiment(dim, n_train, train_seed, loss_fn, 0, checkpoint_dir)
