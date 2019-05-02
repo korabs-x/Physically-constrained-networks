f02b0adaea1d34410be159cbb857715f01016511
KorbinianBstrtr <ga84can@mytum.de>
2019-05-02 12:00:18 +0200

    next experiments

--- experiments/rotation/round2_det_weights.py
+++ experiments/rotation/round2_det_weights.py
@@ -27,17 +27,11 @@ n_runs = 20
 
 
 def mp_worker(data):
-    dim, det_weight, train_seed = data
+    dim, det_weight, n_train = data
 
-    train_range_spec = train_range
-    if dim == 2:
-        train_range_spec = range(10, 21, 1)
-    if dim == 3:
-        train_range_spec = range(20, 201, 20)
-
-    for n_train in train_range_spec:
+    for train_seed in range(1683, 1683 + n_runs, 1):
         checkpoint_dir = 'checkpoints/'
-        checkpoint_dir += 'round2_detweight2/'
+        checkpoint_dir += 'round2_detweight4/'
         checkpoint_dir += 'dim-{}_detweight-{}_ntrain-{}_seed-{}/'.format(dim, det_weight, n_train, train_seed)
         loss_fn = [{'loss_fn': lossfn.get_mse_loss(), 'weight': 1, 'label': 'mse'},
                    {'loss_fn': lossfn.get_det_loss(), 'weight': det_weight, 'label': 'det'}]
@@ -46,10 +40,16 @@ def mp_worker(data):
 
 
 def mp_handler():
-    for dim in [2, 3]:
+    for dim in [3]:
         for det_weight in det_weights:
-            for train_seed in range(1683, 1683+n_runs, 1):
-                p = multiprocessing.Process(target=mp_worker, args=((dim, det_weight, train_seed),))
+            train_range_spec = train_range
+            if dim == 2:
+                train_range_spec = range(10, 21, 1)
+            if dim == 3:
+                train_range_spec = range(20, 201, 20)
+
+            for n_train in train_range_spec:
+                p = multiprocessing.Process(target=mp_worker, args=((dim, det_weight, n_train),))
                 p.start()
 
 
