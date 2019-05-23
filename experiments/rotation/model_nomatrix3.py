59ba0464164b672f509c083d924b3878f4c6b510
KorbinianBstrtr <ga84can@mytum.de>
2019-05-23 10:25:35 +0200

    next experiments

--- experiments/rotation/model_nomatrix3.py
+++ experiments/rotation/model_nomatrix3.py
@@ -43,3 +43,24 @@ class NetNomatrix16(nn.Module):
     def forward(self, x_batch):
         return self.ff(x_batch)
 
+
+class NetNomatrix16V2(nn.Module):
+    def __init__(self, dim, n_hidden_nodes=None):
+        super().__init__()
+        torch.manual_seed(0)
+        self.dim = dim
+        hidden_nodes = 16 if n_hidden_nodes is None else n_hidden_nodes
+
+        self.ff = nn.Sequential(
+            nn.Linear(2 * self.dim - 1, hidden_nodes),
+            nn.Tanh(),
+            nn.Linear(hidden_nodes, hidden_nodes),
+            nn.Tanh(),
+            nn.Linear(hidden_nodes, hidden_nodes),
+            nn.Tanh(),
+            nn.Linear(hidden_nodes, self.dim)
+        )
+
+    def forward(self, x_batch):
+        return self.ff(x_batch)
+
